b89b7aa3bb4c91502687bf27f6a88d8d14af1621
patryq03 <138616976+patryq03@users.noreply.github.com>
2023-12-09 16:59:43 +0000

    Deploying to gh-pages from @ patryq03/goit-js-hw-09@aaf08c0b53dde684fcd01c253bbcb757facd0bba 🚀

---
+++ 01-color-switcher.2aca65cf.js
@@ -0,0 +1,2 @@
+!function(){document.querySelector("body");var t,e=document.querySelector("button[data-start]"),n=document.querySelector("button[data-stop]");e.addEventListener("click",(function(){e.setAttribute("disabled","disabled"),t=setInterval((function(){document.body.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}),1e3)})),n.addEventListener("click",(function(){clearInterval(t)}))}();
+//# sourceMappingURL=01-color-switcher.2aca65cf.js.map
